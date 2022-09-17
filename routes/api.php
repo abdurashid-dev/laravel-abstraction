3446fe79a699edd3af98c1f1edc9d60f384384be
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-17 14:27:05 +0500

    Category controller added to api.php

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
+Route::resource('/categories', \App\Http\Controllers\CategoryController::class);
