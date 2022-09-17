95d3ab8b47ec918c4ac2f719fd8d876be50d39aa
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-17 13:57:20 +0500

    basic product CRUD done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = Product::all();
+        return response()->json(['items' => $items]);
+    }
+
+    public function show($id)
+    {
+        $item = Product::findOrFail($id);
+        return response()->json(['item' => $item]);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required'
+        ]);
+        $item = Product::create($data);
+        return response()->json(['item' => $item]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $item = Product::findOrFail($id);
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required'
+        ]);
+        $item->update($data);
+        return response()->json(['item' => $item]);
+    }
+
+    public function destroy($id)
+    {
+        $item = Product::findOrFail($id);
+        $item->delete();
+        return response()->json(['item' => $item]);
+    }
 }
