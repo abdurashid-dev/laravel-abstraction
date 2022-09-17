186417d288455cb4cf464c0918f02154ad00b4f7
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-17 14:39:05 +0500

    productService done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,41 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Product\StoreRequest;
 use App\Http\Requests\Product\UpdateRequest;
+use App\Http\Services\ProductService;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
+    protected $service;
+
+    public function __construct()
+    {
+        $this->service = new ProductService();
+    }
+
     public function index()
     {
-        $items = Product::all();
+        $items = $this->service->index();
         return response()->json(['items' => $items]);
     }
 
     public function show($id)
     {
-        $item = Product::findOrFail($id);
+        $item = $this->service->show($id);
         return response()->json(['item' => $item]);
     }
 
     public function store(StoreRequest $request)
     {
-        $data = $request->validated();
-        $item = Product::create($data);
+        $item = $this->service->store($request->validated());
         return response()->json(['item' => $item]);
     }
 
     public function update(UpdateRequest $request, $id)
     {
-        $item = Product::findOrFail($id);
-        $data = $request->validated();
-        $item->update($data);
+        $item = $this->service->update($request->validated(), $id);
         return response()->json(['item' => $item]);
     }
 
     public function destroy($id)
     {
-        $item = Product::findOrFail($id);
-        $item->delete();
+        $item = $this->service->destroy($id);
         return response()->json(['item' => $item]);
     }
 }
--- app/Http/Services/ProductService.php
+++ app/Http/Services/ProductService.php
@@ -1,10 +1,35 @@
 <?php
+
 namespace App\Http\Services;
+
+use App\Models\Product;
 
 class ProductService
 {
     public function index()
     {
-        
+        return Product::all();
+    }
+
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function store(array $data)
+    {
+        return Product::create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $item = $this->show($id);
+        return $item->update($data);
+    }
+
+    public function destroy($id)
+    {
+        $item = $this->show($id);
+        return $item->delete($id);
     }
 }
